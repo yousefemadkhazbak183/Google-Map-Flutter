8f31f2ff827b8f4563db1f14be8b4ec190e3bcbd
Yousef Emad <yousefemadkhazbak@gmail.com>
2025-07-27 16:59:49 +0300

    - added google_map_flutter plugin. - integrate with Android & iOS. - added Map Screen. - updated for ios deployment.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import Flutter
 import UIKit
+import GoogleMaps
+
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDcy2BaCQIrXNnoxlHQcgmIztwI1zHjC8Q")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
