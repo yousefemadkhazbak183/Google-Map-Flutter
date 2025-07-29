a9a64d3a202e70403f69415924271522748ab7e1
Yousef Emad <yousefemadkhazbak@gmail.com>
2025-07-29 15:30:24 +0300

    - Drawing Routes & PolyLines. - Added flutter_polyine_points package.

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -17,26 +17,15 @@ class MapSampleState extends State<MapScreen> {
       Completer<GoogleMapController>();
 
   static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(30.042316, 31.190980),
-    zoom: 14,
+    target: LatLng(30.10189429846024, 31.374616987875225),
+    zoom: 12,
   );
   String mapStyle = '';
-  Set<Marker> markers = {};
-  StreamSubscription<Position>? positionStream;
-  bool _isPermissionGranted = false;
-  LatLng? _currentLocation;
+  final Set<Polyline> _polylines = {};
 
   @override
   void initState() {
-    _loadMapStyle();
     super.initState();
-    _checkPermissionRequest();
-  }
-
-  @override
-  void dispose() {
-    positionStream?.cancel();
-    super.dispose();
   }
 
   @override
@@ -44,99 +33,43 @@ class MapSampleState extends State<MapScreen> {
     return Scaffold(
       body: GoogleMap(
         myLocationButtonEnabled: true,
-        myLocationEnabled: _isPermissionGranted,
+        myLocationEnabled: true,
         style: mapStyle,
         initialCameraPosition: _kGooglePlex,
+        polylines: _polylines,
         onMapCreated: (GoogleMapController controller) async {
           _controller.complete(controller);
-          _loadMapStyle();
-          markers.addAll({
-            Marker(
-              markerId: const MarkerId('Joe'),
-              position: const LatLng(30.042316, 31.190980),
-              infoWindow: const InfoWindow(title: 'Joe'),
-              icon: await customIcon('assets/car_icon.png'),
-            ),
-            Marker(
-              markerId: const MarkerId('Joe2'),
-              position: const LatLng(30.04568171838561, 31.129671150500652),
-              infoWindow: const InfoWindow(title: 'Nahia', snippet: 'Kerdasa'),
-              icon: await customIcon('assets/car_icon.png'),
-            ),
-          });
-        },
-        markers: markers,
-      ),
-    );
-  }
 
-  void _loadMapStyle() async {
-    final String style = await DefaultAssetBundle.of(
-      context,
-    ).loadString('assets/map_style.json');
-    setState(() {
-      mapStyle = style;
-    });
-  }
-
-  Future<BitmapDescriptor> customIcon(String asset) async {
-    return await BitmapDescriptor.asset(
-      const ImageConfiguration(size: Size(48, 48)),
-      asset,
-    );
-  }
-
-  _checkPermissionRequest() async {
-    PermissionStatus status = await Permission.location.request();
-
-    if (status.isGranted) {
-      setState(() {
-        _isPermissionGranted = true;
-      });
-      _getUserLocation();
-    } else {}
-  }
-
-  void _getUserLocation() async {
-    if (!_isPermissionGranted) {
-      return;
-    }
-    Position position = await Geolocator.getCurrentPosition(
-      locationSettings: const LocationSettings(accuracy: LocationAccuracy.high),
-    );
-    setState(() {
-      _currentLocation = LatLng(position.latitude, position.longitude);
-    });
-    if (_currentLocation == null) return;
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(
-      CameraUpdate.newCameraPosition(
-        CameraPosition(target: _currentLocation!, zoom: 14),
-      ),
-    );
-
-    // Tracking
-    startTracking();
-  }
-
-  void startTracking() {
-    positionStream =
-        Geolocator.getPositionStream(
-          locationSettings: const LocationSettings(
-            accuracy: LocationAccuracy.high,
-          ),
-        ).listen((Position position) {
           setState(() {
-            _currentLocation = LatLng(position.latitude, position.longitude);
-          });
+            _polylines.add(
+              const Polyline(
+                polylineId: PolylineId('Route'),
+                color: Colors.green,
 
-          _controller.future.then((controller) {
-            controller.animateCamera(
-              CameraUpdate.newCameraPosition(
-                CameraPosition(target: _currentLocation!, zoom: 15),
+                points: [
+                  LatLng(30.116190380064282, 31.417552930269608),
+                  LatLng(30.10189429846024, 31.374616987875225),
+                  LatLng(30.129807962623357, 31.36877048398477),
+                ],
               ),
             );
+            _getPolyLine();
           });
-        });
+        },
+      ),
+    );
+  }
+
+  _getPolyLine() async {
+    PolylinePoints polylinePoints = PolylinePoints(
+      apiKey: "AIzaSyDcy2BaCQIrXNnoxlHQcgmIztwI1zHjC8Q",
+    );
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      request: PolylineRequest(
+        origin: const PointLatLng(30.10189429846024, 31.374616987875225),
+        destination: const PointLatLng(30.129807962623357, 31.36877048398477),
+        mode: TravelMode.driving,
+      ),
+    );
   }
 }
